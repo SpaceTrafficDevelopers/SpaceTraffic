07a6dd2eaee57d394452b08dda6b0b815d52c880
Michel Soběhart <michelin46@seznam.cz>
2015-04-12 21:33:08 +0200

    Fixed control of spaceship on planet, added result messages in ShipLoadCargo

--- GameServer/Game/Actions/Ships/ShipLoadCargo.cs
+++ GameServer/Game/Actions/Ships/ShipLoadCargo.cs
@@ -25,53 +25,74 @@ namespace SpaceTraffic.Game.Actions
 {
     class ShipLoadCargo : IGameAction
     {
-        private string result = "Zboží se nakládá.";
+        private string result = "Náklad se nakládá.";
 
-		
-		public object Result
-		{
-			get { return new { result = this.result }; }
-		}
+
+        public object Result
+        {
+            get { return new { result = this.result }; }
+        }
 
         void IGameAction.Perform(IGameServer gameServer)
         {
             getArgumentsFromActionArgs(gameServer);
+
             Cargo cargo = gameServer.Persistence.GetCargoDAO().GetCargoById(CargoID);
             SpaceShip spaceship = gameServer.Persistence.GetSpaceShipDAO().GetSpaceShipById(SpaceShipID);
-            List<SpaceShip> spaceships = gameServer.Persistence.GetSpaceShipDAO().GetSpaceShipsByPlayer(PlayerId);
-            //Base basePlanet = gameServer.Persistence.GetBaseDAO().GetBaseById(spaceship.DockedAtBaseId);
-            String location = gameServer.World.Map[StarSystemName].Planets[PlanetName].Location;
 
-            if (String.Compare(location, StarSystemName + "\\" + PlanetName) != 0) { 
-                //TODO: zboží není na stejný planetě jako 
-            }
-            if (!spaceships.Contains(spaceship))
+            Entities.Base dockedBase = gameServer.Persistence.GetBaseDAO().GetBaseById(spaceship.DockedAtBaseId);
+            Planet planet = gameServer.World.Map[StarSystemName].Planets[PlanetName];
+
+            if (!dockedBase.Planet.Equals(planet))
             {
-                //TODO: loď nepatří hráči, nemůže na ni nakládat
-                // result = String.Format("Loď s ID {0} Vám nepatří", SpaceShipID);
+                result = String.Format("Loď {0} není zadokována na planetě {1}.", spaceship.SpaceShipName, PlanetName);
+                return;
             }
-            else { 
-                SpaceShipCargo spaceshipcargo = new SpaceShipCargo() {
-                    Cargo = cargo,
-                    CargoId = cargo.CargoId,
-                    CargoCount = Count,
-                    SpaceShip = spaceship,
-                    SpaceShipId = spaceship.SpaceShipId
-                };
-                gameServer.Persistence.GetSpaceShipCargoDAO().InsertSpaceShipCargo(spaceshipcargo);
+            if (spaceship.PlayerId != PlayerId)
+            {
+                result = String.Format("Loď {0} Vám nepatří, nemůžete na ní naložit náklad.", spaceship.SpaceShipName);
+                return;
             }
+
+            SpaceShipCargo spaceshipcargo = getSpaceshipCargoFromArguments(gameServer, cargo, spaceship);
+
+            //TODO: Kontrola místa na lodi
+
+            gameServer.Persistence.GetSpaceShipCargoDAO().InsertSpaceShipCargo(spaceshipcargo);
+            result = String.Format("Náklad byl úspěšně naložen na loď s {0}.", spaceship.SpaceShipName);
         }
 
         /// <summary>
         /// Get all arguments to properties from action args.
         /// </summary>
         /// <param name="gameServer">Instance of game server</param>
-        void getArgumentsFromActionArgs(IGameServer gameServer) {
+        private void getArgumentsFromActionArgs(IGameServer gameServer)
+        {
             StarSystemName = ActionArgs[0].ToString();
             PlanetName = ActionArgs[1].ToString();
             SpaceShipID = Convert.ToInt32(ActionArgs[2]);
             CargoID = Convert.ToInt32(ActionArgs[3]);
             Count = Convert.ToInt32(ActionArgs[4]);
+        }
+
+        /// <summary>
+        /// Get spaceship cargo from arguments
+        /// </summary>
+        /// <param name="gameServer">Instance of game server</param>
+        /// <param name="cargo">Instance of cargo for load</param>
+        /// <param name="spaceship">Instance of spaceship</param>
+        /// <returns>Spaceship cargo</returns>
+        private SpaceShipCargo getSpaceshipCargoFromArguments(IGameServer gameServer, Cargo cargo, SpaceShip spaceship)
+        {
+            SpaceShipCargo spaceshipcargo = new SpaceShipCargo()
+            {
+                Cargo = cargo,
+                CargoId = cargo.CargoId,
+                CargoCount = Count,
+                SpaceShip = spaceship,
+                SpaceShipId = spaceship.SpaceShipId
+            };
+            return spaceshipcargo;
         }
 
         public GameActionState State
