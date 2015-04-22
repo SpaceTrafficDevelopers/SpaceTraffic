47792cfaec4a727ce9978c175ae79326a21a54f8
JKoty <jan.kotalik@email.cz>
2015-04-22 14:28:35 +0200

    Added extension for flash messages.

---
+++ GameUi/JS/core/flashInit.js
@@ -0,0 +1,9 @@
+﻿
+//init of flash messages
+;
+$(document).ready(function () {
+	$("#alert_placeholder").flashMessage({
+		timeout: -1,//it never fades out automaticly
+		fadeout: 300 //speed of removing even with clicking on cross
+	});
+});
