7a534fa567a2412c46d4f7c081d4867cc9226dce
Jan Smitka <jan.smitka@lynt.cz>
2015-05-07 22:58:55 +0200

    DAO for GameAction and GameEvent entities, both registered to PersistenceManager. Entities configured in SpaceTrafficContext.

---
+++ GameServer/Dao/IGameActionDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceTraffic.Entities;
+
+namespace SpaceTraffic.Dao
+{
+    /// <summary>
+    /// Operations with persisted game actions.
+    /// </summary>
+    public interface IGameActionDAO
+    {
+        /// <summary>
+        /// Returns ordered list of all actions in the persistence store.
+        /// </summary>
+        /// <returns>List of all actions ordered by their sequence number.</returns>
+        List<GameAction> GetAllActions();
+
+        /// <summary>
+        /// Preforms a bulk-insert of the given actions to the persistence store.
+        /// </summary>
+        /// <param name="gameActions">List of game actions to persist.</param>
+        void InsertActions(IEnumerable<GameAction> gameActions);
+
+        /// <summary>
+        /// Removes all persisted actions.
+        /// </summary>
+        void RemoveAllActions();
+    }
+}
---
+++ GameServer/Dao/IGameEventDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceTraffic.Entities;
+
+namespace SpaceTraffic.Dao
+{
+    /// <summary>
+    /// Operations with persisted game events.
+    /// </summary>
+    public interface IGameEventDAO
+    {
+        /// <summary>
+        /// Returns ordered list of all events in the persistence store.
+        /// </summary>
+        /// <returns>List of all actions ordered by their planned date.</returns>
+        List<GameEvent> GetAllEvents();
+
+        /// <summary>
+        /// Preforms a bulk-insert of the given events to the persistence store.
+        /// </summary>
+        /// <param name="gameActions">List of game events to persist.</param>
+        void InsertEvents(IEnumerable<GameEvent> gameActions);
+
+        /// <summary>
+        /// Removes all persisted events.
+        /// </summary>
+        void RemoveAllEvents();
+    }
+}
