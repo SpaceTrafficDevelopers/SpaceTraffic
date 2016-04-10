b4b997a40b6f5461035e258d13000aca1b94f093
seborj <seborj@students.zcu.cz>
2016-04-10 15:51:00 +0200

    Registration model moved to AccountModels, useless files removed

--- GameUi/Models/AccountModels.cs
+++ GameUi/Models/AccountModels.cs
@@ -57,6 +57,31 @@ namespace SpaceTraffic.GameUi.Models
         public bool RememberMe { get; set; }
     }
 
+    public class RegisterModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
+        [DataType(DataType.Text)]
+        public string NickName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Souhlasím s podmínkami")]
+        public bool Rules { get; set; }
+    }
+
     public class LostPasswordModel
     {
         [Required]
