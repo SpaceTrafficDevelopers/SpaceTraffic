f3e29f1d2c6f7a06f5fe3a06a07ceb7f94c63375
JKoty <jan.kotalik@email.cz>
2016-04-17 18:36:08 +0200

    Buying/selling complete UI.

---
+++ GameUi/JS/game/BuySellScripts.js
@@ -0,0 +1,15 @@
+﻿//this file contains additional functionality for buy and sell forms
+
+$('#cargoBuyForm, #cargoSellForm').on('successSend', function () {
+	setTimeout(function () {
+		$('.refreshButton').click();
+	}, 800);
+});
+//send max amount button
+$('.sendMax').click(function (e) {
+	e.preventDefault();
+	var inp = $(this).parent().find('.buyAmount, .sellAmount');
+	var send = $(this).parent().find('.sendBtn');
+	inp.val(inp.attr('max'));
+	send.click();
+});
